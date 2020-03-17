a5b5c618c6ed9f801f10c64b776f498afcfc9c22
Gustavo <gustavo.costa@thread.com.br>
2020-03-17 17:29:34 -0300

    ajustes nos pageobjects

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -1,8 +1,10 @@
 let userActions = {
   expectLoggedUser: function(nome) {
     return this
-    .waitForElementVisible("@userInfo", 10000)
-    .assert.containsText("@userInfo", nome);
+    .waitForElementPresent("@userInfo", 10000)
+    .assert.containsText("@userInfo", nome)
+    
+    
   }
 };
 
